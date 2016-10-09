1f1cffe6d7ab55f047a268d61b0439f430956e54
Allen Johnson <soccer25536@allens-macbook-pro.local>
2016-10-09 10:12:09 -0500

    Initial Commit

---
+++ NeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNetwork
+//
+//  Created by Allen Johnson on 10/9/16.
+//  Copyright © 2016 ALJ.inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
